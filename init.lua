fcd2ebc397cd8dd822478e0d4a64b6368b984ab6
idkau <idkau@pm.me>
2023-01-11 22:58:05 -0700

    updates

--- init.lua
+++ init.lua
@@ -87,7 +87,7 @@ cmp.setup({
   })
 
   -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['sumneko_lua'].setup {
     capabilities = capabilities
